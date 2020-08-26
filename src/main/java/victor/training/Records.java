b6a46dd70e55c029e6d2ef3ef6d53364e65bd7c1
Victor Rentea <victorrentea@gmail.com>
2020-08-26 14:42:53 +0300

    END

--- src/main/java/victor/training/Records.java
+++ src/main/java/victor/training/Records.java
@@ -1,53 +1,24 @@
 package victor.training;
 
-import java.util.Objects;
-
 public class Records {
    public static void main(String[] args) {
-      System.out.println(intervalsIntersect(new Interval(1, 3), new Interval(2, 4)));
+      System.out.println(new Interval(1, 3).intersects(new Interval(2, 4)));
    }
 
-   public static boolean intervalsIntersect(Interval interval, Interval interval1) {
-      return interval.start() <= interval1.end() && interval1.start() <= interval.end();
-   }
 }
 
-class Interval {
-   private final int start;
-   private final int end;
-
-   public Interval(int start, int end) {
-      this.start = start;
-      this.end = end;
+record Interval(int start, int end) {
+   public Interval {
+      if (start > end) {
+         throw new IllegalArgumentException();
+      }
    }
 
-   public int start() {
-      return start;
+   public Interval(String startStr, int end) {
+      this(Integer.parseInt(startStr), end);
    }
 
-   public int end() {
-      return end;
-   }
-
-   @Override
-   public String toString() {
-      return "Interval{" +
-             "start=" + start +
-             ", end=" + end +
-             '}';
-   }
-
-   @Override
-   public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Interval interval = (Interval) o;
-      return start == interval.start &&
-             end == interval.end;
-   }
-
-   @Override
-   public int hashCode() {
-      return Objects.hash(start, end);
+   public boolean intersects(Interval other) {
+      return start() <= other.end() && other.start() <= end();
    }
 }
